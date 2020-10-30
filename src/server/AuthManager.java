926d782f36818836ddaaa641a8fa6cbd739b2c18
Georg Thomassen <geo.cornelius@gmail.com>
2020-10-31 00:04:10 +0100

    added hashing

--- src/server/AuthManager.java
+++ src/server/AuthManager.java
@@ -2,12 +2,14 @@ package server;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
-import javax.swing.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class AuthManager {
 
@@ -16,9 +18,15 @@ public class AuthManager {
         String password2 = "supersecurepassword";
 
         try {
-            System.out.println(AuthManager.hashPassword(password));
-            System.out.println(AuthManager.hashPassword(password2));
-            System.out.println(AuthManager.hashPassword(password));
+            String test1 = AuthManager.hashPassword2(password);
+            String test2 = AuthManager.hashPassword2(password2);
+            String test3 = AuthManager.hashPassword2(password);
+
+            System.out.println(test1);
+            System.out.println(test2);
+            System.out.println(test3);
+
+            
         } catch (Exception e) {
             System.err.println("Couldn't hash password");
             e.printStackTrace();
@@ -39,5 +47,27 @@ public class AuthManager {
         byte[] hash = factory.generateSecret(spec).getEncoded();
 
         return Arrays.toString(hash);
+    }
+
+    private static String hashPassword2(String password) {
+        char[] passwordChars = password.toCharArray();
+        byte[] passwordBytes = password.getBytes();
+
+        try {
+            SecureRandom random = new SecureRandom();
+            byte[] salt = new byte[16];
+            random.nextBytes(salt);
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(salt);
+
+            final byte[] salted = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+            return Base64.getEncoder().encodeToString(salted);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
